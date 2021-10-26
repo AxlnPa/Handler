1ea72e955e75ff1a8adf1a6021e961a37c2e48d2
AxlnPa <83622481+AxlnPa@users.noreply.github.com>
2021-10-26 14:21:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: 32767,
+});
+module.exports = client;
+
+// Variables globales
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+// Inicia tu bot
+require("./handler")(client);
+
+client.login(client.config.token);
